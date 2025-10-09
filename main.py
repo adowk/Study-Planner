43e2a0325a1ade7cd38d18e9ba9d2201f28b0a80
adowk <adowk1929@uis.no>
2025-10-09 12:31:59 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-import functions.py as funksjon
+import functions as f
+
+if __name__ == "__main__":
+    main()
+
+
+while True:
+        if valg == "1":
+            f.lag_emne()
+        elif valg == "2":
+            f.legg_til_emne()
