63cbd15c49472304b4b96262fe448afaec3830b4
adowk <adowk1929@uis.no>
2025-10-09 12:54:36 +0200

    Update functions.py

--- functions.py
+++ functions.py
@@ -8,6 +8,16 @@ def lag_emne():
   s = input("Høst eller vår: ").capitalize()
   p = int(input("Studiepoeng: "))
 
+  def skriv_ut_emner():
+    if len(emnekoder) == 0:
+        print("Ingen emner registrert ennå.")
+    else:
+        for i in range(len(emnekoder)):
+            print(f"{i+1}. {emnekoder[i]} ({semestere[i]}) - {studiepoeng[i]} stp")
+
+ def legg_til_emne():
+   skriv_ut_emner()
+  
   emnekoder.append(k)
   semestere.append(s)
   studiepoeng.append(p)
