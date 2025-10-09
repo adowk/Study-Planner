e0e9e7c3d68a885af8865be989f150eefb7c2da0
adowk <adowk1929@uis.no>
2025-10-09 13:16:04 +0200

    Update functions.py

--- functions.py
+++ functions.py
@@ -5,12 +5,12 @@ studiepoeng = []
 studieplan = [[], [], [], [], [], []]
 
 def lag_emne():
-  k = input("Emnekode: ").upper()
-  s = input("Høst eller vår: ").capitalize()
-  p = int(input("Studiepoeng: "))
-  emnekoder.append(k)
-  semestere.append(s)
-  studiepoeng.append(p)
+  kode = input("Emnekode: ").upper()
+  semester = input("Høst eller vår: ").capitalize()
+  poeng = int(input("Studiepoeng: "))
+  emnekoder.append(kode)
+  semestere.append(semester)
+  studiepoeng.append(poeng)
 
   def skriv_ut_emner():
     if len(emnekoder) == 0:
@@ -21,8 +21,8 @@ def lag_emne():
 
 def legg_til_emne():
   skriv_ut_emner()
- e = int(input("Hvilket emne vil du legge til? "))
- s = int(input("I hvilket semester vil du legge det til (1-6)? "))
- emnekoder.append(e)
- semestere.append(s)
+  e = int(input("Hvilket emne vil du legge til? "))
+  s = int(input("I hvilket semester vil du legge det til (1-6)? "))
+  emnekoder.append(e)
+  semestere.append(s)
  
