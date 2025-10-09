f1211c7b0845550e233ccea0d2b84b7abb9788d9
henrikengel-bit <henrik.engelsvoll@gmail.com>
2025-10-09 13:10:15 +0200

    funk
    some

--- functions.py
+++ functions.py
@@ -1,4 +1,4 @@
-import shit
+
 emnekoder = []
 semestere = []
 studiepoeng = []
@@ -8,6 +8,9 @@ def lag_emne():
   k = input("Emnekode: ").upper()
   s = input("Høst eller vår: ").capitalize()
   p = int(input("Studiepoeng: "))
+  emnekoder.append(k)
+  semestere.append(s)
+  studiepoeng.append(p)
 
   def skriv_ut_emner():
     if len(emnekoder) == 0:
@@ -16,9 +19,10 @@ def lag_emne():
         for i in range(len(emnekoder)):
             print(f"{i+1}. {emnekoder[i]} ({semestere[i]}) - {studiepoeng[i]} stp")
 
- def legg_til_emne():
-   skriv_ut_emner()
-  
-  emnekoder.append(k)
-  semestere.append(s)
-  studiepoeng.append(p)
+def legg_til_emne():
+  skriv_ut_emner()
+ e = int(input("Hvilket emne vil du legge til? "))
+ s = int(input("I hvilket semester vil du legge det til (1-6)? "))
+ emnekoder.append(e)
+ semestere.append(s)
+ 
--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 import functions as f
 
-if __name__ == "__main__":
-    main()
 
+def main():
+    while True:
+            print("1. Lag emne")
+            print("2. Legg til emne")
+            print("3. Skriv ut emner")
+            print("4. Avslutt")
+            valg = input("Velg et alternativ: ")
 
-while True:
-        if valg == "1":
-            f.lag_emne()
-        elif valg == "2":
-            f.legg_til_emne()
+            if valg == "1":
+                f.lag_emne()
+            elif valg == "2":
+                f.legg_til_emne()
+            elif valg == "3":
+                f.skriv_ut_emner()
+            elif valg == "4":
+                break
+
+main()
