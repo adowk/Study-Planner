36690df2e13d0017d0885ed5470cd33dfd5f5d24
adowk <adowk1929@uis.no>
2025-10-12 18:11:30 +0200

    Update functions.py

--- functions.py
+++ functions.py
@@ -3,27 +3,30 @@ semestere = []
 studiepoeng = []
 studieplan = [[], [], [], [], [], []]
 
-def lag_emne():
-    kode = input("Emnekode: ").upper()
-    semester = input("Høst eller vår: ").capitalize()
-    poeng = int(input("Studiepoeng: "))
+def lag_nytt_emne(kode, sem_type, sp):
+    if kode in emnekoder:
+        print(f"Emnet {kode} finnes allerede.")
+        return
     emnekoder.append(kode)
-    semestere.append(semester)
-    studiepoeng.append(poeng)
+    semester_type.append(sem_type)
+    studiepoeng.append(sp)
 
-def skriv_ut_emner():
-    if len(emnekoder) == 0:
-        print("Ingen emner registrert ennå.")
-    else:
-        for i in range(len(emnekoder)):
-            print(f"{i+1}. {emnekoder[i]} ({semestere[i]}) - {studiepoeng[i]} stp")
-#TESTS
-def legg_til_emne():
-    skriv_ut_emner()
-    e = int(input("Hvilket emne vil du legge til? ")) - 1
-    s = int(input("I hvilket semester vil du legge det til (1-6)? ")) - 1
-    if 0 <= e < len(emnekoder) and 0 <= s < len(studieplan):
-        studieplan[s].append(emnekoder[e])
-        print(f"{emnekoder[e]} lagt til i semester {s+1}")
-    else:
-        print("Ugyldig valg.")
+
+def legg_til_i_studieplan(kode, semester):
+    if kode not in emnekoder:
+        print("Emnet finnes ikke.")
+        return
+    indeks = emnekoder.index(kode)
+    semtype = semester_type[indeks]
+    sp = studiepoeng[indeks]
+
+    host_sem = [1, 3, 5]
+    var_sem = [2, 4, 6]
+
+    for s in studieplan:
+        if kode in s:
+            print(f"Emnet {kode} er allerede i studieplanen.")
+            return    for s in studieplan:
+        if kode in s:
+            print(f"Emnet {kode} er allerede i studieplanen.")
+            return
