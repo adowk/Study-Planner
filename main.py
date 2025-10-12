07cbb198ca2a4e36c122544af1ac1d9b491dedca
adowk <adowk1929@uis.no>
2025-10-12 20:58:25 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,3 @@
-from functions import lag_emne, legg_til_emne, skriv_ut_emner, studieplan
+from functions import lag_nytt_emne, legg_til_i_studieplan
 
-while True:
-    print("\n1. Registrer nytt emne")
-    print("2. Vis emner")
-    print("3. Legg til emne i studieplan")
-    print("4. Vis studieplan")
-    print("5. Avslutt")
-
-    valg = input("Velg handling: ")
-
-    if valg == "1":
-        lag_emne()
-    elif valg == "2":
-        skriv_ut_emner()
-    elif valg == "3":
-        legg_til_emne()
-    elif valg == "4":
-        for i, semester in enumerate(studieplan, start=1):
-            print(f"Semester {i}: {semester}")
-    elif valg == "5":
-        break
-    else:
-        print("Ugyldig valg.")
-#TEST
+# TODO: Legg til meny (Input og output for bruker å legge til emner osv)
