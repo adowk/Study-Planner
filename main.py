9c0974f01491b6ecd1ff19e2f3a90c4ef93ed128
adowk <adowk1929@uis.no>
2025-10-13 21:57:41 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ from functions import lag_nytt_emne, legg_til_i_studieplan
 
 # Input fra bruker for å bruke funksjonene våres. Disse funksjonene er:
 # Sette if statement for hver funksjon og få input for funksjonene
-
+# GI relevant output inkl funksjonene for if statements
